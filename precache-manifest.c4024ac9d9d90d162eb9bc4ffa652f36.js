15a75f5702cc80f3b7f10dc81c7d83ef9f6b7684
Arpit Bansal <arpit.bansal@outlook.com>
2020-10-16 20:18:22 +0530

    Updates

---
+++ precache-manifest.c4024ac9d9d90d162eb9bc4ffa652f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8b92b6e6ab6b468358cb9abfbd68c1",
+    "url": "/stocks-app/index.html"
+  },
+  {
+    "revision": "bca0dde6cbce46d58a68",
+    "url": "/stocks-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a5ecc267f04b3d0ac20",
+    "url": "/stocks-app/static/js/2.3188b46b.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/stocks-app/static/js/2.3188b46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca0dde6cbce46d58a68",
+    "url": "/stocks-app/static/js/main.035d098b.chunk.js"
+  },
+  {
+    "revision": "86faaa82d9ce8bb464d3",
+    "url": "/stocks-app/static/js/runtime-main.b4dab2dc.js"
+  }
+]);
